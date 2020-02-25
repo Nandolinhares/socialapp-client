b7a217617b7d080b47a9738f7671797bbbe1b1df
Nando Linhares <fernandolinhares1920@globo.com>
2020-02-25 15:47:21 -0300

    initializing login form

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles';
 import Link from 'react-router-dom/Link';
 
-//MUI Stuff
+//MUI Stuffs
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
